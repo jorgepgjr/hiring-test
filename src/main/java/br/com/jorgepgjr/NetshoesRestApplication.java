b1615723b0f03e144e9af657a5e1f7f8fcf68ab7
Jorge Peres Guimarães Junior <jorgepgjr@gmail.com>
2016-06-20 18:08:06 -0300

    create the structure for a Spring Boot App

---
+++ src/main/java/br/com/jorgepgjr/NetshoesRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.jorgepgjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetshoesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetshoesRestApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/jorgepgjr/NetshoesRestApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.jorgepgjr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NetshoesRestApplication.class)
+@WebAppConfiguration
+public class NetshoesRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
